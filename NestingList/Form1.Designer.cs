28366f12ca0fabaf9a784931eaa1acb9cde41ac2
Marius K. <marius.into@gmail.com>
2020-11-05 19:58:12 +0200

    Add project files.

---
+++ NestingList/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace NestingList
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PartsList = new System.Windows.Forms.ListView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PartsList
+            // 
+            this.PartsList.Location = new System.Drawing.Point(11, 8);
+            this.PartsList.Name = "PartsList";
+            this.PartsList.Size = new System.Drawing.Size(987, 507);
+            this.PartsList.TabIndex = 0;
+            this.PartsList.UseCompatibleStateImageBehavior = false;
+            this.PartsList.View = System.Windows.Forms.View.List;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 534);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(80, 28);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import...";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(98, 534);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 28);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 583);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.PartsList);
+            this.Name = "MainForm";
+            this.Text = "Nesting List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView PartsList;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ NestingList/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NestingList
+{
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            value = textBox.Text;
+            return dialogResult;
+        }
+
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Title = "Pasirinkite";
+            openFileDialog1.Filter = "TPA nesting files (*.ncad)|*.ncad";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                string value = "1";
+                if (InputBox("Multiply", "Quantity:", ref value) == DialogResult.OK)
+                {
+                    //MessageBox.Show(selectedFileName + ": " + value);
+
+                    // https://stackoverflow.com/questions/642293/how-do-i-read-and-parse-an-xml-file-in-c
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(selectedFileName);
+
+                    /* foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+                    {
+                        string text = node.Name; //  .InnerText; //or loop through its children as well
+                        MessageBox.Show(text);
+                    } */
+
+                    var xml = XDocument.Load(@selectedFileName);
+                    var query = from c in xml.Root.Descendants("rows")
+                                select c.Element("row").Attribute("name").Value;
+
+
+                    foreach (string item in query)
+                    {
+                        MessageBox.Show(item);
+                    }
+                }
+            }
+        }
+    }
+}
